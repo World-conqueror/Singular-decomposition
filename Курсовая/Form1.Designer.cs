bca1860b9712d1cda5de8a2624ba8f6d916e23e4
Илья <@LAPTOP-MTTOJDVQ>
2017-06-09 16:07:59 +0300

    Добавьте файлы проекта.

---
+++ Курсовая/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Курсовая
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        #region Код, автоматически созданный конструктором форм Windows
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(151, 55);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(124, 22);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 55);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(124, 22);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество строк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 34);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Количество столбцов";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(307, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Создать матрицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(266, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите размеры матрицы :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 60);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Разложить матрицу по сингулярным числам";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(570, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 60);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Сокращённое сингулярное разложение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(696, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 60);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Полное сингулярное разложение";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(822, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 60);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Стереть данные";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 453);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Сингулярное разложение";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.MaskedTextBox[,] Matrix_A;
+        private System.Windows.Forms.MaskedTextBox[] Matrix_W;
+        private System.Windows.Forms.MaskedTextBox[,] Matrix_U;
+        private System.Windows.Forms.MaskedTextBox[,] Matrix_VT;
+        #endregion
+    }
+}
---
+++ Курсовая/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows.Forms;
+namespace Курсовая
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int m; private int n; private int x; private int y; private int d = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = Convert.ToInt32(numericUpDown1.Value);
+            n = Convert.ToInt32(numericUpDown2.Value);
+            Matrix_A = new MaskedTextBox[m, n]; d = 1;
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    MaskedTextBox newMaskedTextBox = new MaskedTextBox(); x = 10 + j * 30;
+                    newMaskedTextBox.Name = "newMaskedTextBox";
+                    Matrix_A[i, j] = newMaskedTextBox;
+                    newMaskedTextBox.Location = new System.Drawing.Point(x, 80 + i * 20);
+                    newMaskedTextBox.Size = new System.Drawing.Size(30, 20);
+                    Controls.Add(newMaskedTextBox);
+                }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] A = new double[m, n];
+                double[] W = new double[Math.Min(m, n)];
+                double[,] U = new double[m, n];
+                double[,] VT = new double[m, n];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        A[i, j] = Convert.ToDouble(Matrix_A[i, j].Text);
+                alglib.svd.rmatrixsvd(A, m, n, 0, 0, 2, ref W, ref U, ref VT);
+                Matrix_W = new MaskedTextBox[Math.Min(m, n)]; d = 2;
+                for (int i = 0; i < Math.Min(m, n); i++)
+                {
+                    MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                    newMaskedTextBox.Name = "newMaskedTextBox";
+                    Matrix_W[i] = newMaskedTextBox;
+                    Matrix_W[i].Text = Convert.ToString(W[i]);
+                    newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + i * 50, 80);
+                    newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                    Controls.Add(newMaskedTextBox);
+                }
+            }
+            catch (System.FormatException) { }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] A = new double[m, n];
+                double[] W = new double[Math.Min(m, n)];
+                double[,] U = new double[m, n];
+                double[,] VT = new double[m, n];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        A[i, j] = Convert.ToDouble(Matrix_A[i, j].Text);
+                alglib.svd.rmatrixsvd(A, m, n, 1, 1, 2, ref W, ref U, ref VT);
+                Matrix_W = new MaskedTextBox[Math.Min(m, n)];
+                for (int i = 0; i < Math.Min(m, n); i++)
+                {
+                    MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                    newMaskedTextBox.Name = "newMaskedTextBox";
+                    Matrix_W[i] = newMaskedTextBox;
+                    Matrix_W[i].Text = Convert.ToString(W[i]);
+                    newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + i * 50, 80);
+                    newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                    Controls.Add(newMaskedTextBox);
+                }
+                Matrix_U = new MaskedTextBox[m, Math.Min(m, n)];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < Math.Min(m, n); j++)
+                    {
+                        MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                        newMaskedTextBox.Name = "newMaskedTextBox";
+                        Matrix_U[i, j] = newMaskedTextBox;
+                        Matrix_U[i, j].Text = Convert.ToString(U[i, j]); y = 120 + i * 20;
+                        newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + j * 50, y);
+                        newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                        Controls.Add(newMaskedTextBox);
+                    }
+                Matrix_VT = new MaskedTextBox[Math.Min(m, n), n]; d = 3;
+                for (int i = 0; i < Math.Min(m, n); i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                        newMaskedTextBox.Name = "newMaskedTextBox";
+                        Matrix_VT[i, j] = newMaskedTextBox;
+                        Matrix_VT[i, j].Text = Convert.ToString(VT[i, j]);
+                        newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + j * 50, y + 40 + i * 20);
+                        newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                        Controls.Add(newMaskedTextBox);
+                    }
+            }
+            catch (System.FormatException) { }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double[,] A = new double[m, n];
+                double[] W = new double[Math.Min(m, n)];
+                double[,] U = new double[m, n];
+                double[,] VT = new double[m, n];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < n; j++)
+                        A[i, j] = Convert.ToDouble(Matrix_A[i, j].Text);
+                alglib.svd.rmatrixsvd(A, m, n, 2, 2, 2, ref W, ref U, ref VT);
+                Matrix_W = new MaskedTextBox[Math.Min(m, n)];
+                for (int i = 0; i < Math.Min(m, n); i++)
+                {
+                    MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                    newMaskedTextBox.Name = "newMaskedTextBox";
+                    Matrix_W[i] = newMaskedTextBox;
+                    Matrix_W[i].Text = Convert.ToString(W[i]);
+                    newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + i * 50, 80);
+                    newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                    Controls.Add(newMaskedTextBox);
+                }
+                Matrix_U = new MaskedTextBox[m, m];
+                for (int i = 0; i < m; i++)
+                    for (int j = 0; j < m; j++)
+                    {
+                        MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                        newMaskedTextBox.Name = "newMaskedTextBox";
+                        Matrix_U[i, j] = newMaskedTextBox;
+                        Matrix_U[i, j].Text = Convert.ToString(U[i, j]); y = 120 + i * 20;
+                        newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + j * 50, y);
+                        newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                        Controls.Add(newMaskedTextBox);
+                    }
+                Matrix_VT = new MaskedTextBox[n, n]; d = 4;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                    {
+                        MaskedTextBox newMaskedTextBox = new MaskedTextBox();
+                        newMaskedTextBox.Name = "newMaskedTextBox";
+                        Matrix_VT[i, j] = newMaskedTextBox;
+                        Matrix_VT[i, j].Text = Convert.ToString(VT[i, j]);
+                        newMaskedTextBox.Location = new System.Drawing.Point(x + 50 + j * 50, y + 40 + i * 20);
+                        newMaskedTextBox.Size = new System.Drawing.Size(50, 20);
+                        Controls.Add(newMaskedTextBox);
+                    }
+            }
+            catch (System.FormatException) { }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch (d)
+            {
+                case 1:
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < n; j++)
+                            Controls.Remove(Matrix_A[i, j]);
+                    d = 0; break;
+                case 2:
+                    for (int i = 0; i < Math.Min(m, n); i++)
+                        Controls.Remove(Matrix_W[i]);
+                    d = 1; break;
+                case 3:
+                    for (int i = 0; i < Math.Min(m, n); i++)
+                        Controls.Remove(Matrix_W[i]);
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < Math.Min(m, n); j++)
+                            Controls.Remove(Matrix_U[i, j]);
+                    for (int i = 0; i < Math.Min(m, n); i++)
+                        for (int j = 0; j < n; j++)
+                            Controls.Remove(Matrix_VT[i, j]);
+                    d = 1; break;
+                case 4:
+                    for (int i = 0; i < Math.Min(m, n); i++)
+                        Controls.Remove(Matrix_W[i]);
+                    for (int i = 0; i < m; i++)
+                        for (int j = 0; j < m; j++)
+                            Controls.Remove(Matrix_U[i, j]);
+                    for (int i = 0; i < n; i++)
+                        for (int j = 0; j < n; j++)
+                            Controls.Remove(Matrix_VT[i, j]);
+                    d = 1; break;
+            }
+        }
+    }
+}
